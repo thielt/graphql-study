f8dcbadbcb65650f9baa787891edc2f20e9f9427
Thiel Talicuran <thieltalicuran@Thiels-MBP.attlocal.net>
2022-08-31 11:03:22 -0500

    first

---
+++ go.mod
@@ -0,0 +1,7 @@
+module graphqlstudy
+
+go 1.19
+
+require github.com/graphql-go/graphql v0.8.0 // direct
+
+require github.com/graphql-go/handler v0.2.3 // direct
---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+//setting up the server
+import (
+	"net/http"
+
+	"github.com/graphql-go/handler"
+)
+
+func main() {
+	h := handler.New(&handler.Config{
+		Schema:   &BeastSchema,
+		Pretty:   true,
+		GraphiQL: false,
+	})
+
+	//original endpoint
+	http.Handle("/graphql", h)
+
+	//write our html to /sandbox endpoint in url
+	http.Handle("/sandbox", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Write(sandboxHTML)
+	}))
+
+	http.ListenAndServe(":8080", nil)
+
+}
+
+// embedding sandbox
+// apollo sandbox
+var sandboxHTML = []byte(`
+<!DOCTYPE html>
+<html lang="en">
+<body style="margin: 0; overflow-x: hidden; overflow-y: hidden">
+<div id="sandbox" style="height:100vh; width:100vw;"></div>
+<script src="https://embeddable-sandbox.cdn.apollographql.com/_latest/embeddable-sandbox.umd.production.min.js"></script>
+<script>
+ new window.EmbeddedSandbox({
+   target: "#sandbox",
+   // Pass through your server href if you are embedding on an endpoint.
+   // Otherwise, you can pass whatever endpoint you want Sandbox to start up with here.
+   initialEndpoint: "http://localhost:8080/graphql",
+ });
+ // advanced options: https://www.apollographql.com/docs/studio/explorer/sandbox#embedding-sandbox
+</script>
+</body>
+</html>`)
